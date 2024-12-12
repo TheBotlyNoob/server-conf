97db093f44bed5ee0b1eed7ff38554301a043548
TheBotlyNoob <jj@jjay.dev>
2024-12-11 22:27:18 -0500

    whoops

--- headscale/exit-node/container/init.sh
+++ headscale/exit-node/container/init.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-/usr/local/bin/headscale users create exit-node-user >>/logs/headscale-create-user.txt 2>&1
+/usr/local/bin/headscale users create exit-node-user
 
-TS_AUTHKEY="$(/usr/local/bin/headscale preauthkeys create --ephemeral -e 15m -o json -u exit-node-user 2>>/logs/headscale-preauth.txt | jq -r '.key')"
+TS_AUTHKEY="$(/usr/local/bin/headscale preauthkeys create --ephemeral -e 15m -o json -u exit-node-user | jq -r '.key')"
 export TS_AUTHKEY
 
 export TS_EXTRA_ARGS="$TS_EXTRA_ARGS --login-server=http://$HEADSCALE_HOST"
 
-/usr/local/bin/containerboot >>/logs/tailscale.txt 2>&1
+/usr/local/bin/containerboot
