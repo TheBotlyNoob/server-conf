18c013a3215e962043e393924253aa36c7fa479d
TheBotlyNoob <jj@jjay.dev>
2024-12-12 17:02:22 -0500

    fiix

--- headscale/headscale/container/init.sh
+++ headscale/headscale/container/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[ -f "/var/cache/headplane/api-key.txt" ] || /usr/local/bin/headscale apikey create -e 9999d -o yaml >/var/cache/headscale/api-key.txt
+[ -f "/var/cache/headplane/api-key.txt" ] || /usr/local/bin/headscale apikey create -e 9999d -o yaml >/var/cache/headplane/api-key.txt
 
 ROOT_API_KEY=$(cat /var/cache/headplane/api-key.txt)
 export ROOT_API_KEY
