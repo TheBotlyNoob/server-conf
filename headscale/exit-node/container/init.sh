f168f7139eaafedfb46e9843b0f97670446e9128
TheBotlyNoob <jj@jjay.dev>
2024-12-12 16:56:34 -0500

    cache stuff

--- headscale/exit-node/container/init.sh
+++ headscale/exit-node/container/init.sh
@@ -2,7 +2,9 @@
 
 /usr/local/bin/headscale users create exit-node-user
 
-TS_AUTHKEY="$(/usr/local/bin/headscale preauthkeys create --ephemeral -e 15m -o json -u exit-node-user | jq -r '.key')"
+[ -f "/var/cache/tailscale/preauth-key.txt" ] || /usr/local/bin/headscale preauthkeys create --ephemeral -e 15m -o json -u exit-node-user | jq -r '.key' >/var/cache/tailscale/preauth-key.txt
+
+TS_AUTHKEY="$(cat /var/cache/tailscale/preauth-key.txt)"
 export TS_AUTHKEY
 
 export TS_EXTRA_ARGS="$TS_EXTRA_ARGS --login-server=https://$HEADSCALE_HOST"
--- headscale/headscale/container/init.sh
+++ headscale/headscale/container/init.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-ROOT_API_KEY=$(/usr/local/bin/headscale apikey create -e 9999d -o yaml)
+[ -f "/var/cache/headplane/api-key.txt" ] || /usr/local/bin/headscale apikey create -e 9999d -o yaml >/var/cache/headscale/api-key.txt
+
+ROOT_API_KEY=$(cat /var/cache/headplane/api-key.txt)
 export ROOT_API_KEY
 
 node ./build/headplane/server.js
