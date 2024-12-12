937e82315e4953502b996de91552fcba6db5bd2b
TheBotlyNoob <jj@jjay.dev>
2024-12-11 21:51:46 -0500

    update init script

--- headscale/exit-node/container/init.sh
+++ headscale/exit-node/container/init.sh
@@ -5,6 +5,9 @@
 TS_AUTHKEY="$(/usr/local/bin/headscale preauthkeys create --ephemeral -e 15m -o json -u exit-node-user | jq -r '.key')"
 export TS_AUTHKEY
 
-export TS_TAILSCALED_EXTRA_ARGS="$TAILSCALE_EXTRA_ARGS --login-server=headscale:8080"
+export TS_EXTRA_ARGS="$TS_EXTRA_ARGS --login-server=headscale:8080"
+
+echo "TS_AUTHKEY: $TS_AUTHKEY"
+echo "TS_EXTRA_ARGS: $TS_EXTRA_ARGS"
 
 /usr/local/bin/containerboot
