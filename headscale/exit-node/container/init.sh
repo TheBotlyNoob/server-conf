ab1f7a4b9c64e81173e4be6804a290b8dfbcd7c0
TheBotlyNoob <jj@jjay.dev>
2024-12-11 22:09:36 -0500

    add wireguard exit node

--- headscale/exit-node/container/init.sh
+++ headscale/exit-node/container/init.sh
@@ -5,6 +5,6 @@
 TS_AUTHKEY="$(/usr/local/bin/headscale preauthkeys create --ephemeral -e 15m -o json -u exit-node-user | jq -r '.key')"
 export TS_AUTHKEY
 
-export TS_EXTRA_ARGS="$TS_EXTRA_ARGS --login-server=http://headscale:8080"
+export TS_EXTRA_ARGS="$TS_EXTRA_ARGS --login-server=http://$HEADSCALE_HOST"
 
 /usr/local/bin/containerboot
