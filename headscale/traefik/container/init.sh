67c935571bd6d5515733c96b805e025b1ba1428f
TheBotlyNoob <jj@jj.is-a.dev>
2024-12-15 16:54:15 -0500

    hmm

---
+++ headscale/traefik/container/init.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+SUBDOMAINS=$(echo "$PROXY_SUBDOMAINS" | tr "," "\n")
+
+for SUBDOMAIN in $SUBDOMAINS; do
+    PORT=$(echo "$SUBDOMAIN" | cut -d':' -f2)
+    SUBDOMAIN=$(echo "$SUBDOMAIN" | cut -d':' -f1)
+
+    cat <<EOF >/rules/"$SUBDOMAIN-subdomain-rev-proxy".yml
+http:
+    routers:
+        $SUBDOMAIN-subdomain-rev-proxy-rtr:
+            rule: HostRegexp(\`^(.+\.)?$SUBDOMAIN\.{{ env "DOMAINNAME" | regexQuoteMeta }}$\`)
+            service: $SUBDOMAIN-subdomain-rev-proxy-svc
+    services:
+        $SUBDOMAIN-subdomain-rev-proxy-svc:
+            loadBalancer:
+                servers:
+                    - url: "http://frps-internal:$PORT"
+EOF
+
+    TRAEFIK_ENTRYPOINTS_WEBSECURE_HTTP_TLS_DOMAINS_0_SANS=",*.$SUBDOMAIN.$DOMAINNAME"
+done
+
+export TRAEFIK_ENTRYPOINTS_WEBSECURE_HTTP_TLS_DOMAINS_0_SANS
+
+traefik
