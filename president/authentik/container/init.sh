61086a3c8ed3b2158bceafd41b96c8854eb6813c
TheBotlyNoob <jj@jjay.dev>
2024-12-23 16:16:08 -0500

    mmmkay

--- president/authentik/container/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# change `default=Themes.AUTOMATIC` to `default=Themes.DARK` in `/authentik/brands/api.py`
-
-# see https://stackoverflow.com/questions/77256594
-sed 's/default=Themes\.AUTOMATIC/default=Themes\.DARK/g' /authentik/brands/api.py >/authentik/brands/api.py.tmp
-mv /authentik/brands/api.py.tmp /authentik/brands/api.py
-
-dumb-init -- ak server
