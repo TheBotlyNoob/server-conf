066f65c62cafe377ab01cd23ed3912fbb435997f
TheBotlyNoob <jj@jjay.dev>
2024-12-23 16:22:29 -0500

    fix homepage

--- president/homepage/container/init.sh
+++ president/homepage/container/init.sh
@@ -5,8 +5,7 @@ SUBDOMAINS=$(echo "$PROXY_SUBDOMAINS" | tr "," "\n")
 cp -R /config /app
 
 for SUBDOMAIN in $SUBDOMAINS; do
-    DOCKER_SOCKET_PORT=$(echo "$SUBDOMAIN" | cut -d':' -f3)
-    _PORT=$(echo "$SUBDOMAIN" | cut -d':' -f2)
+    DOCKER_SOCKET_PORT=$(echo "$SUBDOMAIN" | cut -d':' -f2)
     SUBDOMAIN=$(echo "$SUBDOMAIN" | cut -d':' -f1)
 
     cat <<EOF >>/app/config/docker.yaml
