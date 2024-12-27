30ce3b762952e8df093f6a53baf262fb6d8812c9
TheBotlyNoob <jj@jjay.dev>
2024-12-27 00:46:25 -0500

    fix synapse, too

--- president/matrix/cinny/container/init.sh
+++ president/matrix/cinny/container/init.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-echo "$DOMAINNAME"
+if [ -f /app/config.json ]; then
+    echo "Config file already exists, skipping"
+    exit 0
+fi
 
 (
     echo "cat <<EOF"
     cat /config.template.json
     echo EOF
 ) | sh >/app/config.json
-
-cat /app/config.json
--- president/matrix/synapse/container/init.sh
+++ president/matrix/synapse/container/init.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ -f /config/homeserver.yaml ]; then
+    echo "Config file already exists, skipping"
+    exit 0
+fi
+
 (
     echo "cat <<EOF"
     cat /homeserver.template.yaml
