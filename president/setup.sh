b22755fb59cd4e174222f845188f1a033a7836a5
TheBotlyNoob <jj@jjay.dev>
2025-01-10 22:38:11 -0500

    add minor check

--- president/setup.sh
+++ president/setup.sh
@@ -7,6 +7,16 @@ set -e
 # shellcheck source=/dev/null
 . .env
 
+if [ -z "$PINGVIN_STORAGE_SIZE_MEGABITS" ]; then
+    echo "PINGVIN_STORAGE_SIZE_MEGABITS is not set"
+    exit 1
+fi
+
+if [ -z "$APP_ROOT" ]; then
+    echo "APP_ROOT is not set"
+    exit 1
+fi
+
 disk_path="$APP_ROOT/pingvin/data"
 mkdir -p "$disk_path"
 chown -R "$PUID:$PGID" "$disk_path"
